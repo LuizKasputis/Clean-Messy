ace290589d35d05c36c7b88d8acf8d1d0ad0a55b
Luiz Zanini <luiz.zanini@scalait.com>
2021-04-02 15:16:25 -0300

    add cookiecutter to project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data science project that  predicts when the room it's clean or messy',
+    author='Luiz Guilherme Zanini',
+    license='MIT',
+)
